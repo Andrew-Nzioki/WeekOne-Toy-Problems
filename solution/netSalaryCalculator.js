247b2bff26ddf9db695279742a363d978d099721
Andrew Kyalo Nzioki <andrewkyalonzioki001.com>
2023-03-18 23:50:23 +0300

    split the challenge 3 into tiny segments and solved one by one

--- solution/netSalaryCalculator.js
+++ solution/netSalaryCalculator.js
@@ -1,71 +1,40 @@
 
-const personalRelief=2400;
-const insuranceRelief=5000;
-const AllowablePensionFundContribution=20000;
-const allowableHOSPContribution=0;
-const AffordableHousingRelief=9000;
-const AllowableOwnerOccupierInterest=25000;
-const DisabilityExemption=150000;
-function CalculatePAYE(Salary) {
-  let PAYE;
-  if (Salary <= 24000) {
-    PAYE = Salary * 0.01;
-  } else if ((Salary > 24000 && Salary <= 32333)) {
-    PAYE = Salary * 0.25;
-  } else if ((Salary > 32333)) {
+//a function to calculate PAYE
+function calculateTax(grossSalary, contributionBenefit, disability=false, haveMortgage=false, mortgageInterest=0, haveLifeInsurancePolicy=false, insurancePremium=0, homeOwnershipSavings=false, homeOwnershipTotalDeposit=0) {
+
+  let taxableIncome = grossSalary - contributionBenefit
+
+  if (disability) {
+    taxableIncome = taxableIncome * 0.75
   }
-  return PAYE;
-}
 
-// Calculate NHIF deductions
-function CalculateNhif(grossPay) {
-  let nhif;
-  if (grossPay <= 5999) {
-    nhif = 150;
-  } else if (grossPay <= 7999) {
-    nhif = 300;
-  } else if (grossPay <= 11999) {
-    nhif = 400;
-  } else if (grossPay <= 14999) {
-    nhif = 500;
-  } else if (grossPay <= 19999) {
-    nhif = 600;
-  } else if (grossPay <= 24999) {
-    nhif = 750;
-  } else if (grossPay <= 29999) {
-    nhif = 850;
-  } else if (grossPay <= 34999) {
-    nhif = 900;
-  } else if (grossPay <= 39999) {
-    nhif = 950;
-  } else if (grossPay <= 44999) {
-    nhif = 1000;
-  } else if (grossPay <= 49999) {
-    nhif = 1100;
-  } else if (grossPay <= 59999) {
-    nhif = 1200;
-  } else if (grossPay <= 69999) {
-    nhif = 1300;
-  } else if (grossPay <= 79999) {
-    nhif = 1400;
-  } else if (grossPay <= 89999) {
-    nhif = 1500;
-  } else if (grossPay <= 99999) {
-    nhif = 1600;
-  } else {
-    nhif = 1700;
+  if (haveMortgage) {
+    taxableIncome = taxableIncome - mortgageInterest
   }
-  return nhif;
+
+  if (haveLifeInsurancePolicy) {
+    taxableIncome = taxableIncome - insurancePremium
+  }
+
+  if (homeOwnershipSavings) {
+    taxableIncome = taxableIncome - homeOwnershipTotalDeposit
+  }
+
+ 
+  const annualIncome = paymentFrequency === 'monthly' ? taxableIncome * 12 : taxableIncome
+
+  let taxPayable = 0
+
+  if (annualIncome > 2) {
+    const incomeAboveThreshold = annualIncome - taxRates[yearOfTaxation].threshold
+    taxPayable = incomeAboveThreshold * taxRates[yearOfTaxation].rate
+  }
+
+  return taxPayable
 }
 
-console.log(CalculateNhif(24000))
+const taxPayable = calculateTax(2022, 'monthly', 50000, 1000, true, true, 5000, true, 1000, true, 2000)
+console.log(taxPayable)
 
-//This function takes your yearly deduction and returns the total deductions made in a year based on tier batch
-function CalculateNSSF(nssfdeduction){
-if(nssfdeduction>0 && nssdeduction <72000){
-    totaldeductions=grossPay*0.06
-//this else condition takes care of people using tire II model to make their nssf deductions 
-}else if(nssfdeduction>72001&& nssdeduction <216801){
 
-}
-}
+
---
+++ solution/taxRate.js
@@ -0,0 +1,13 @@
+function calculateTaxRate(grossSalary){
+let taxRates;
+if(grossSalary>=24000&&grossSalary<=288000){
+  return taxRates=0.01
+}else if(grossSalary>288000&&grossSalary<=388000){
+  return taxRates=0.25
+}else {
+  return taxRates=0.30
+}
+}
+console.log(calculateTaxRate(299999))
+
+module.exports=calculateTaxRate(grossSalary);
